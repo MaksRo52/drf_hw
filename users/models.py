6a705ca4bf4301d66590a2a03473a02f46330d04
m.romantsov <romancov_dzr@mail.ru>
2024-11-01 17:52:30 +0300

    create base

---
+++ users/models.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Email")
+    phone_number = PhoneNumberField(
+        null=True, blank=True, verbose_name="Номер телефона"
+    )
+    country = models.CharField(
+        max_length=50, verbose_name="Страна", null=True, blank=True
+    )
+    photo = models.ImageField(
+        upload_to="users/photos/",
+        verbose_name="Фото",
+        help_text="Загрузите своё фото.",
+        null=True,
+        blank=True,
+    )
+    token = models.CharField(
+        max_length=100, verbose_name="Токен", null=True, blank=True
+    )
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        permissions = [
+            ("can_edit_is_active", "Может блокировать пользователя"),
+            ("can_view_users", "Может просматривать пользователей"),
+        ]
+
+    def __str__(self):
+        return self.email
