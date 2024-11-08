c9df285b93a91cfd5a34dd7390134035375eb821
m.romantsov <romancov_dzr@mail.ru>
2024-11-08 18:03:06 +0300

    task 3

--- lms/views.py
+++ lms/views.py
@@ -1,11 +1,13 @@
 from rest_framework.generics import (CreateAPIView, DestroyAPIView,
                                      ListAPIView, RetrieveAPIView,
                                      UpdateAPIView)
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from lms.models import Course, Lesson
 from lms.serializers import (CourseDetailSerializer, CourseSerializer,
                              LessonSerializer)
+from users.permissions import IsModer, IsOwner
 
 
 class CourseViewSet(ModelViewSet):
@@ -16,10 +18,20 @@ class CourseViewSet(ModelViewSet):
             return CourseDetailSerializer
         return CourseSerializer
 
+    def get_permissions(self):
+        if self.action == "create":
+            self.permission_classes = (~IsModer,)
+        elif self.action in ["update", "retrieve"]:
+            self.permission_classes = (IsModer | IsOwner,)
+        elif self.action == "destroy":
+            self.permission_classes = (~IsModer | IsOwner,)
+        return super().get_permissions()
+
 
 class LessonCreateApiView(CreateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated,~IsModer]
 
 
 class LessonListAPIView(ListAPIView):
@@ -35,9 +47,11 @@ class LessonRetrieveAPIView(RetrieveAPIView):
 class LessonUpdateAPIView(UpdateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated, IsModer]
 
 
 class LessonDestroyAPIView(DestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated, ~IsModer]
 
---
+++ users/permissions.py
@@ -0,0 +1,18 @@
+from rest_framework import permissions
+
+class IsModer(permissions.BasePermission):
+    """
+        Проверка на права Модератора.
+        """
+    def has_permission(self, request, view):
+        return request.user.groups.filter(name="moders").exists()
+
+
+class IsOwner(permissions.BasePermission):
+    """
+    Проверка на владельца.
+    """
+    def has_object_permission(self, request, view, obj):
+        if obj.owner == request.user:
+            return True
+        return False
